be5c993a744933ba3c75b576b3be2a76ba74b926
yousefedapedGlass <100702015+yousefedapedGlass@users.noreply.github.com>
2022-08-10 12:59:24 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a=1;
+    int b=5;
+    int c=25;
+
+    double x=5.4;
+    double y=6.2;
+    double z=12.4;
+
+    System.out.println("a is "+a);
+    System.out.println("b is "+b);
+    System.out.println("c is "+c);
+  }
+}
